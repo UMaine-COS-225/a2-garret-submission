f067e43d6de0acbaf237aaf3bdf97a5a381e0018
0xR <114037840+0xRum@users.noreply.github.com>
2024-09-23 01:20:13 -0400

    cleaned and added
    shortened some string names and made corrective actions

--- TextAnalyzer.java
+++ TextAnalyzer.java
@@ -2,13 +2,13 @@ import java.util.HashMap;
 
 public class TextAnalyzer{
     public static void main(String[] args){
-        if (args.length != 3){
-            System.out.println("This is java TextAnalyzer <inputFile> <charFrequencyFile> <wordFrequencyFile> ");
+        if (args.length != 3) {
+            System.out.println("This is java TextAnalyzer <inputFile> <charFreqFile> <wordFreqFile>");
             return;
         }
         String inputFile = args[0];
-        String charFrequencyFile = args[1];
-        String wordFrequencyFile = args[2];
+        String charFreqFile = args[1];
+        String wordFreqFile = args[2];
 
         String content = readFile(inputFile);
 
@@ -16,33 +16,48 @@ public class TextAnalyzer{
         System.out.println(countWords(content));
         System.out.println(countLines(content));
         
-        HashMap<Character, Integer> charFrequency = countCharacterFrequency(content);
-        writeCharacterFrequency(charFrequency, charFrequencyFile);
+        HashMap<Character, Integer> charFreq = countCharacterFrequency(content);
+        writeCharacterFrequency(charFreq, charFreqFile);
 
-        HashMap<String, Integer> wordFrequency = countWordFrequency(content);
-        writeWordFrequency(wordFrequency, wordFrequencyFile);
+        HashMap<String, Integer> wordFreq = countWordFrequency(content);
+        writeWordFrequency(wordFreq, wordFreqFile);
 
     }
 
-    public static int countCharacters(String fileName){
+        public static String readFile(String fileName) {
+        String content = "";
+        try {
+            content = new String(java.nio.file.Files.readAllBytes(java.nio.file.Paths.get(fileName)));
+        } catch (java.io.IOException e) {
+            System.out.println("File not found: " + fileName);
+        }
+        return content;
+    }
+
+    public static int countCharacters(String content){
         return content.length();
     }
-    public static int countWords(String fileName){
-
+    
+    public static int countWords(String content){
+        String[] words = content.split("\\s+");
+        return words.length;
     }
-    public static int countLines(String fileName){
-
+    
+    public static int countLines(String content){
+        String[] lines = content.split("\n");
+        return lines.length;
     }
-    public static int countCharacterFrequency(String fileName){
+    // public static int countCharacterFrequency(String content){
 
-    }
-    public static int countWordFrequency(String fileName){
+    // }
+    // public static int countWordFrequency(String content){
 
-    }
-    public static int writeCharacterFrequency(String fileName, int frequency){
+    // }
+    public static HashMap<Character, Integer> charFreq = countCharacterFrequency(String content);{
+        return charFreq;
 
     }
     public static int writeWordFrequency(String fileName, int frequency){
-
+        return 0;
     }
 }
